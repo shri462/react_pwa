baece143bfe5cf3325280e392c3081e9aba6ee5e
Shrinivas Biradar <shrinivas.biradar@techdome.net.in>
2023-10-21 23:50:10 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import "./App.css";
+import { useState } from "react";
+import { fetchWeather } from "./api/fetchWeather";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [weatherData, setWeatherData] = useState({});
+
+  const search = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeatherData(data);
+      setQuery("");
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        name="query"
+        id="query"
+        placeholder="Search..."
+        className="search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={search}
+      />
+
+      {weatherData.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weatherData.name}</span>
+            <sup>{weatherData.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weatherData.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+              alt={weatherData.weather[0].description}
+            />
+            <p>{weatherData.weather[0].description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
