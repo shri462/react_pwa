d7acd5ddba74b3ccbe87b4b1d683a272b4508d47
Shrinivas Biradar <shrinivas.biradar@techdome.net.in>
2023-10-22 02:31:28 +0530

    updating service worker to show offline msg immediatly

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -21,6 +21,14 @@ self.addEventListener("fetch", (event) => {
   );
 });
 
+self.addEventListener("offline", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
 // Activate the SW
 self.addEventListener("activate", (event) => {
   const cacheWhitelist = [];
